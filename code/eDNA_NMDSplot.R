02b01f1d096a0d526f4b6c16200f6095b574279e
NickJeff13 <Nick.w.jeffery3@gmail.com>
2024-06-05 15:53:45 -0300

    Updating NMDS script and adding PCOA plots

--- code/eDNA_NMDSplot.R
+++ code/eDNA_NMDSplot.R
@@ -1,7 +1,9 @@
 library(vegan)
+library(ape)
 library(dplyr)
 library(janitor)
 library(ggplot2)
+library(ggrepel)
 library(vroom)
 
 specs <- read.csv(file = "data/12S_Taxonomy_Table_FilteredFinal_grouped.csv", header = T)
@@ -12,7 +14,7 @@ sort(unique(specs$Sp))
 
 commat <- specs[,3:48]
 groups<- c(rep("DOT",27), rep("SmithRoot",19))
-
+weeks<-c(rep(1:9,3), 1,2,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9)
 #Transpose the table for vegan
 commat2<-t(commat)
 colnames(commat2)<-specs[,1] #this selects the 3rd column from our taxon table and inserts the species IDs into this matrix
@@ -32,7 +34,7 @@ orditorp(dot.nmds, display="sites", cex=1, air=0.01)
 
 #plot NMDS with ggplot to look nicer
 data.scores <- as.data.frame(scores(dot.nmds,"sites")) %>%
-  mutate(ID=rownames(.), group=groups)
+  mutate(ID=rownames(.), group=as.factor(groups))
 species.scores <- as.data.frame(scores(dot.nmds, "species"))  #Using the scores function from vegan to extract the species scores and convert to a data.frame
 species.scores$species <- rownames(species.scores) 
 
@@ -68,7 +70,7 @@ p1
 
 
 ggsave(filename = "DOT_NMDS.png", plot = p1, device = "png", path = "figures/", width = 16, height=12, units = "in", dpi=400, bg = "white")
-
+ggsave(filename = "DOT_NMDS_12S_byWeek.png", plot = p1, device = "png", path = "figures/", width = 16, height=12, units = "in", dpi=400, bg = "white")
 
 #Species accumulation curve and other info 
 gg<- specaccum(commat2, method="exact", ci.type="polygon")
@@ -83,7 +85,7 @@ ggplot()+geom_line(data=ggg, aes(x=gg.sites,y=gg.richness),colour="navyblue", lw
 ggsave(filename = "DOT_specaccum.png", plot = last_plot(), device = "png", path = "figures/", width = 14, height=10, units = "in", dpi=400, bg = "white")
 
 #rarefaction
-trask.rare<-rarecurve(commat2, step=10)
+dot.rare<-rarecurve(commat2, step=10)
 
 
 #this gives us Shannon index values per sample 
@@ -91,7 +93,19 @@ shan <- data.frame(diversity(commat2, index="shannon"))
 #we can aggregate these so we get a site level index
 mean.shan <- aggregate(. ~ substr(rownames(shan), 1, 3), shan, mean, na.rm = TRUE)
 
+##Try a PCOA instead of NMDS
+#first transform the species matrix to a dist object
+dist<- vegdist(commat2, method="jaccard")
+dot.pcoa <- ape::pcoa(dist)
+barplot(dot.pcoa$values$Relative_eig[1:10])
 
+df1 <- data.frame(sample=rownames(dot.pcoa$vectors), pc1=dot.pcoa$vectors[,1],pc2=dot.pcoa$vectors[,2],group=groups)
+biplot(dot.pcoa, commat2)
+
+ggplot(df1)+
+  #geom_point(data = df1, aes(x=pc1, y=pc2,color=group))+
+  geom_text_repel(aes(x=pc1,y=pc2,label=sample,color=group))+
+  theme_bw()
 ########################################################################################
 ##Now the COI data
 ##################
@@ -100,6 +114,7 @@ coi <-read.csv("data/DOTCOI_feature_table_export_filtered.csv")
 #36 x 53 dataframe 
 coi.commat <- coi[,7:length(colnames(coi))]
 groups<- c(rep("DOT",27), rep("SmithRoot",20))
+weekz<-c(rep(1:9,3), 1,2,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9)
 
 
 #Transpose the table for vegan
@@ -120,7 +135,7 @@ orditorp(dot.nmds.coi, display="sites", cex=1, air=0.01)
 
 #plot NMDS with ggplot to look nicer
 data.scores.coi <- as.data.frame(scores(dot.nmds.coi,"sites")) %>%
-  mutate(ID=rownames(.), group=groups)
+  mutate(ID=rownames(.), group=as.factor(weekz))
 species.scores.coi <- as.data.frame(scores(dot.nmds.coi, "species"))  #Using the scores function from vegan to extract the species scores and convert to a data.frame
 species.scores.coi$species <- rownames(species.scores.coi) 
 
@@ -138,7 +153,7 @@ p2 <- ggplot() +
   #scale_colour_manual()
   geom_text(data=data.scores.coi,aes(x=NMDS1,y=NMDS2, label=ID), size=6)+
   #scale_color_gradient(low="blue", high="black")+
-  scale_colour_brewer(palette = "Accent") +
+  #scale_colour_brewer(palette = "Accent") +
   coord_equal()+
   #scale_shape_manual(values=c(15,7,18,16,10,8,17))+
   geom_text(aes(x=Inf,y=-Inf,hjust=1.05,vjust=-0.5,label=paste("Stress =",round(dot.nmds.coi$stress,3),"k =",dot.nmds.coi$ndim)))+
@@ -147,7 +162,7 @@ p2
 
 
 ggsave(filename = "DOT_NMDS_COI.png", plot = p2, device = "png", path = "figures/", width = 16, height=12, units = "in", dpi=400, bg = "white")
-
+ggsave(filename = "DOT_NMDS_COI_byWeek.png", plot = p2, device = "png", path = "figures/", width = 16, height=12, units = "in", dpi=400, bg = "white")
 
 #Species accumulation curve and other info 
 gg<- specaccum(coi.commat2, method="exact", ci.type="polygon")
@@ -213,14 +228,21 @@ p3
 ggsave(filename = "DOT_NMDS_COI.png", plot = p2, device = "png", path = "figures/", width = 16, height=12, units = "in", dpi=400, bg = "white")
 
 
+##Try a PCOA instead of NMDS
+#first transform the species matrix to a dist object
+dist3<- vegdist(coi.commat3, method="bray")
+dot.pcoa.coi <- ape::pcoa(dist3)
+barplot(dot.pcoa.coi$values$Relative_eig[1:10])
 
+df3 <- data.frame(sample=rownames(dot.pcoa.coi$vectors), pcoa1=dot.pcoa.coi$vectors[,1],pcoa2=dot.pcoa.coi$vectors[,2],group=groups2)
+biplot(dot.pcoa.coi, coi.commat3)
 
-
-
-
-
-
-
+p4<- ggplot(df3)+
+  #geom_point(data = df1, aes(x=pc1, y=pc2,color=group))+
+  geom_text_repel(aes(x=pcoa1,y=pcoa2,label=sample,color=group))+
+  theme_bw()
+p4
+ggsave(filename = "DOT_PCOA_COI_bray.png", plot = p4, device = "png", path = "figures/", width = 16, height=12, units = "in", dpi=400, bg = "white")
 
 save.image("data/DOTeDNA_CommunityStats.RData")
 
